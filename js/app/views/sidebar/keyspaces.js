ce50e86e795cb13b01a322cd90b11d692c09715e
AnthonyBobsin <bobsinj@gmail.com>
2015-07-13 16:54:19 -0400

    Adds some sidebar styles, and toggle-collapse for keyspace tables

--- js/app/views/sidebar/keyspaces.js
+++ js/app/views/sidebar/keyspaces.js
@@ -5,20 +5,48 @@ define(['TablesView'], function(TablesView) {
     initialize: function(options) {
       this.options = options
       this.collection = this.model.get("tables")
+      this.presenterModel = new Backbone.Model({
+        isCollapsed: true
+      })
     },
 
     template: Handlebars.templates['sidebar/keyspaces_layout'],
+
+    className: 'keyspace',
 
     childView: TablesView,
 
     childViewContainer: '.tables-container',
 
     events: {
-
+      'click .collapse-toggle': 'collapseTables'
     },
 
-    bindings: {
+    presenterBindings: {
+      'span.table-collapse': {
+        attributes: [{
+          observe: 'isCollapsed',
+          name: 'class',
+          onGet: function(isCollapsed) {
+            return isCollapsed ? "glyphicon-chevron-right" : "glyphicon-chevron-down"
+          }
+        }]
+      },
 
+      '.tables-container': {
+        attributes: [{
+          observe: 'isCollapsed',
+          name: 'class',
+          onGet: function(isCollapsed) {
+            return isCollapsed ? "hide" : ""
+          }
+        }]
+      }
+    },
+
+    collapseTables: function(e) {
+      e.stopPropagation()
+      this.presenterModel.set("isCollapsed", !this.presenterModel.get("isCollapsed"))
     },
 
     onDestroy: function() {
@@ -26,9 +54,9 @@ define(['TablesView'], function(TablesView) {
     },
 
     render: function() {
-      this.unstickit()
+      this.unstickit(this.presenterModel, this.presenterBindings)
       Backbone.Marionette.CompositeView.prototype.render.call(this)
-      this.stickit()
+      this.stickit(this.presenterModel, this.presenterBindings)
     }
   })
 
